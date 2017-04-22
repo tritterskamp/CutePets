433652a9b336808be045d90ea7172acc8d276e5e
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-04-22 13:46:56 -0500

    fixing error

--- lib/cute_pets/pet_fetcher.rb
+++ lib/cute_pets/pet_fetcher.rb
@@ -34,7 +34,7 @@ module PetFetcher
         pic:   get_photo(pet_json),
         link:  "https://www.petfinder.com/petdetail/#{pet_json['id']['$t']}",
         name:  pet_json['name']['$t'].capitalize,
-        description: [get_petfinder_option(pet_json['options']), get_petfinder_sex(pet_json['sex']['$t']),  get_petfinder_breed(pet_json['breeds'])].compact.join(' ').downcase, pet_json['animal']['$t'].downcase]
+        description: [get_petfinder_option(pet_json['options']), get_petfinder_sex(pet_json['sex']['$t']),  get_petfinder_breed(pet_json['breeds'])].compact.join(' ').downcase, pet_json['animal']['$t'].downcase
       }
     else
       raise 'PetFinder api request failed'
