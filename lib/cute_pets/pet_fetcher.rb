5a29c5a5c414713c37bbefaafdcfcd0b885d3947
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-04-23 14:03:13 -0500

    remove animal-type from description

--- lib/cute_pets/pet_fetcher.rb
+++ lib/cute_pets/pet_fetcher.rb
@@ -34,7 +34,7 @@ module PetFetcher
         pic:   get_photo(pet_json),
         link:  "https://www.petfinder.com/petdetail/#{pet_json['id']['$t']}",
         name:  pet_json['name']['$t'].capitalize,
-        description: [get_petfinder_option(pet_json['options']), get_petfinder_sex(pet_json['sex']['$t']),  get_petfinder_breed(pet_json['breeds']), get_petfinder_animal(pet_json['animal']['$t'])].compact.join(' ').downcase
+        description: [get_petfinder_option(pet_json['options']), get_petfinder_sex(pet_json['sex']['$t']),  get_petfinder_breed(pet_json['breeds'])].compact.join(' ').downcase
       }
     else
       raise 'PetFinder api request failed'
